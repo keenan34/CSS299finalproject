b06639222bba88e5ed6b0d720e48f7448cd46131
CoolBeanie <94817412+CoolBeanie@users.noreply.github.com>
2022-11-14 15:13:42 -0600

    Update bigrams.py

--- bigrams.py
+++ bigrams.py
@@ -23,4 +23,26 @@ def compute_stopwords(texts: List[str]) -> Set[str]:
     return stopwords
 #Identify Bigrams: find all bigrams from filtered tokenize documents AND find all bigrams in the query (in the search)
 #-one way we can identify them is finding bigrams that have - (dashes) in them i.e. pre-school
+
+#Document counts
+def compute_document_counts(texts: List[str]) -> Counter:
+    counts = Counter()
+    for text in texts:
+        # unique_tokens = set(tokenize(text))
+        # for token in unique_tokens:
+        #     counts[token] += 1
+        counts.update(set(tokenize(text)))
+    return counts
+#Counter based text counters
+class CounterBasedTextCounter(TextCounter):
+    @classmethod
+    def count_characters(cls, text: str) -> Dict[str, int]:
+        counts = Counter()
+        counts.update(text)
+        return counts
+
+    def count_words(self, text: str) -> Dict[str, int]:
+        counts = Counter()
+        counts.update(tokenize(text))
+        return counts
 #Rank Bigrams based on their relevance to the query
